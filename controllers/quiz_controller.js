5f9102407af57f3a932db3c9e6856e2e66395295
WillianZamalloa <WillianZamalloa@users.noreply.github.com>
2015-08-06 20:44:53 -0500

    Quiz 14 Borrar Preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -118,5 +118,11 @@ exports.update = function(req, res){
 				}			//Redireccion HTTP a lista de preguntas  (URL relativo)
 			}
 		);
+};
 
+//DELETE /quizes/:id/
+exports.destroy = function(req, res){
+		req.quiz.destroy().then( function(){
+			res.redirect('/quizes');
+		}).catch(function(error){ next(error)});
 };
