a01335d882e602e8e5c78b2d92de9d4f3b99c0b6
WillianZamalloa <WillianZamalloa@users.noreply.github.com>
2015-08-15 10:35:00 -0500

    QUIZ 15 CREAR COMENTARIO

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -2,8 +2,10 @@ var models = require('../models/models.js');
 
 //Autoload - factoriza el codigo si ruta incluye :quizId
 exports.load = function(req, res, next, quizId){
-	models.Quiz.findById(quizId).then(
-		function(quiz){
+	models.Quiz.find({
+				where: { id: Number(quizId)},
+				include : [{model: models.Comment }]
+			}).then(function(quiz){
 			if (quiz) {
 				req.quiz = quiz;
 				next();
@@ -104,7 +106,7 @@ exports.edit = function(req, res){
 exports.update = function(req, res){
 		req.quiz.pregunta = req.body.quiz.pregunta;
 		req.quiz.respuesta = req.body.quiz.respuesta;
-		
+
 		req.quiz.tema = req.body.quiz.tema;
 
 		req.quiz
--- models/models.js
+++ models/models.js
@@ -30,7 +30,17 @@ var sequelize = new Sequelize(DB_name, user, pwd,
 var quiz_path = path.join(__dirname,'quiz');
 var Quiz = sequelize.import(quiz_path);
 
+// Importar la definicion de la tabla Comment
+
+var comment_path = path.join(__dirname,'comment');
+var Comment = sequelize.import(comment_path);
+
+//Relaciones entre  objetos
+Comment.belongsTo(Quiz);
+Quiz.hasMany(Comment);
+
 exports.Quiz = Quiz;//exportar tabla Quiz
+exports.Comment = Comment;
 
 //sequelize.sync() crea e inicializa ta la de preguntas de la DB
 //sequelize.sync().success(function(){ //anterior version
