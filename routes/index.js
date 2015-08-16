211e3a01160c792d10c5060643f0a251e3929a67
WillianZamalloa <WillianZamalloa@users.noreply.github.com>
2015-08-15 19:58:47 -0500

    Quiz 16 Autenticacion y Sesion

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
 var commentController = require('../controllers/comment_controller');
+var sessionController = require('../controllers/session_controller');
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz BillPaz', errors: [] });
@@ -16,6 +17,11 @@ router.get('/quizes/answer',quizController.answer);
 //Autoload de comandos con :quizId
 router.param('quizId', quizController.load); //autoload: quizId
 
+//Definicion de rutas de session
+router.get('/login',    sessionController.new);
+router.post('/login',   sessionController.create);
+router.get('/logout',   sessionController.destroy);
+
 //Definicion de Rutas de /quizes
 router.get('/quizes',                      quizController.index);
 router.get('/quizes/:quizId(\\d+)',        quizController.show);
@@ -26,9 +32,12 @@ router.get('/quizes/:quizId(\\d+)/edit',   quizController.edit);
 router.put('/quizes/:quizId(\\d+)',        quizController.update);
 router.delete('/quizes/:quizId(\\d+)',     quizController.destroy);
 
+
+//Definicion de Rutas de /comments
 router.get('/quizes/:quizId(\\d+)/comments/new',   commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments',       commentController.create);
 
+////Definicion de Rutas de /author
 router.get('/author', function(req, res) {
   res.render('author', { errors: [] });
 });
