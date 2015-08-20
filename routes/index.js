05365bedd3b899b0aba835723b010ec587f9cee0
WillianZamalloa <WillianZamalloa@users.noreply.github.com>
2015-08-20 10:24:23 -0500

    Quiz login y otros 19

--- routes/index.js
+++ routes/index.js
@@ -16,6 +16,7 @@ router.get('/quizes/answer',quizController.answer);
 
 //Autoload de comandos con :quizId
 router.param('quizId', quizController.load); //autoload: quizId
+router.param('commentId', commentController.load); //autoload: quizId
 
 //Definicion de rutas de session
 router.get('/login',    sessionController.new);
@@ -36,6 +37,7 @@ router.delete('/quizes/:quizId(\\d+)',     sessionController.loginRequired, quiz
 //Definicion de Rutas de /comments
 router.get('/quizes/:quizId(\\d+)/comments/new',   commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments',       commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish',   sessionController.loginRequired, commentController.publish);
 
 ////Definicion de Rutas de /author
 router.get('/author', function(req, res) {
